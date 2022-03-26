13a9e64c563f3b97baee7c8064e339529170e8c3
Nicolas Rios <nicolas.rios@icareweb.com>
2022-03-26 19:01:16 +0100

    Add style for `LoginScreen`

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,79 @@
-import React from 'react'
+import React from 'react';
+import './login.css';
 
 export const LoginScreen = () => {
     return (
-        <div>LoginScreen</div>
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-6 login-form-1">
+                    <h3>Login</h3>
+                    <form>
+                        <div className="form-group mb-3">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="submit"
+                                className="btnSubmit"
+                                value="Login"
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>Sign Up</h3>
+                    <form>
+                        <div className="form-group mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Repeat password"
+                            />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <input
+                                type="submit"
+                                className="btnSubmit"
+                                value="Sign Up" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
