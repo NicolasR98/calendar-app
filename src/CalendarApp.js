34079f7766a9395551ba39db899206229a0088d0
Nicolas Rios <nicolas.rios@icareweb.com>
2022-03-27 20:40:55 +0200

    Add initial config for `react-redux`

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import { AppRouter } from './routers/AppRouter'
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
+import { AppRouter } from './routers/AppRouter';
+
 import './styles.css';
 
 export const CalendarApp = () => {
     return (
-        <AppRouter />
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
     );
 };
