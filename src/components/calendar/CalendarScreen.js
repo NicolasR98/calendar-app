c6eb2c6b40f022caf86a321680e66b380a2ed572
Nicolas Rios <nicolas.rios@icareweb.com>
2022-03-27 15:05:48 +0200

    Add `CalendarEvent`

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
+
 import { Navbar } from '../ui/Navbar';
+import { CalendarEvent } from './CalendarEvent';
+import { messages } from '../../helpers/calendar-messages-es';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'moment/locale/es';
-import { messages } from '../../helpers/calendar-messages-es';
 
 
 moment.locale('es');
@@ -16,10 +18,29 @@ const events = [
         title: 'Birthday',
         start: moment().toDate(),
         end: moment().add(2, 'hours').toDate(),
+        user: {
+            _id: '123',
+            name: 'Nicolas',
+        }
     },
 ];
 
 export const CalendarScreen = () => {
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    };
+
+    const onSelectEvent = (e) => {
+        console.log(e);
+    };
+
+    const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    };
+
     const eventStyleGetter = (event, start, end, isSelected) => {
         const style = {
             backgroundColor: '#367CF7',
@@ -37,7 +58,7 @@ export const CalendarScreen = () => {
     return (
         <div className='calendar-screen'>
             <Navbar />
-            <h1>CalendarScreen</h1>
+
             <Calendar
                 localizer={localizer}
                 events={events}
@@ -45,6 +66,13 @@ export const CalendarScreen = () => {
                 endAccessor='end'
                 messages={messages}
                 eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEvent
+                }}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                view={lastView}
             />
         </div>
     );
