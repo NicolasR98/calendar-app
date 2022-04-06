b0bdfcc09bab77fa18960dc45fa3c6c57186686e
Nicolas Rios <nicolas.rios@icareweb.com>
2022-04-06 20:20:08 +0200

    Set up initial auth functionality

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,13 @@ export const types = {
     calendarClearActive: '[calendar] Clear active event',
     calendarUpdate: '[calendar] Update event',
     calendarDelete: '[calendar] Delete event',
+
+    authCheck: '[auth] Checking login state',
+    authCheckFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start register',
+    authRegister: '[auth] Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
 };
