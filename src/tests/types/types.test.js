57090b22d3d4485ed718e8f234fa5703c60f006d
Nicolas Rios <nicolas.rios@icareweb.com>
2022-04-13 17:50:30 +0200

    Add tests on `types.js`

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,29 @@
+import { types } from "../../types/types";
+
+const mockTypes = {
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    calendarStartAddNew: '[calendar] Start add new event',
+    calendarAddNew: '[calendar] Add new event',
+    calendarSetActive: '[calendar] Set active event',
+    calendarClearActive: '[calendar] Clear active event',
+    calendarUpdate: '[calendar] Update event',
+    calendarDelete: '[calendar] Delete event',
+    calendarLoad: '[calendar] Load events',
+    calendarClearAll: '[calendar] Clear all',
+
+    authCheckFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start register',
+    authRegister: '[auth] Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+};
+
+describe('Tests on types.js', () => {
+    test('should match with mockTypes', () => {
+        expect(types).toEqual(mockTypes);
+    });
+});
